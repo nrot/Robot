06431295fcd2692be7f45d4f68dd09e8118b9549
nrot <nrot13@gmail.com>
2015-05-10 23:30:27 +0300

    Add sleep 1 minute.

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,14 @@ public class Main {
             Field.CreateRobot("First", 10, 10, "x+");
             classSlotInvCoord Item1 = new classSlotInvCoord(11, 11, 2, 0, 1);
             Field.PlaceItem(Item1);
+            Field.PlaceItem(new classSlotInvCoord(9, 9, 3, 10, 1));
             Field.DrawPlane();
+            long timeSleep =(long) (double) Math.pow(10, 3)*60;
+            try {
+                Thread.sleep(timeSleep);                 //1000 milliseconds is one second.
+            } catch(InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
         }
     }
 }
